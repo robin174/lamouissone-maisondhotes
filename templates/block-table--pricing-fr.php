c19f001f46d861b0f2c15ad271fea833a264b879
Robin Green <robin@designingreen.co.uk>
2024-07-01 12:09:03 +0100

    Update prices for 24/25

--- templates/block-table--pricing-fr.php
+++ templates/block-table--pricing-fr.php
@@ -7,36 +7,32 @@
 		</thead>
 		<tbody>
 			<tr>
-			    <td><strong>Standard 2024</strong><br>01 janvier 24 - 22 mars 24*</td>
-			    <td>290&euro; par nuitée</td> 
-			</tr>
-			<tr>
-			    <td><strong>Printemps 2024</strong><br>23 mars 24 - 31 mai 24*</td>
-			    <td>330&euro; par nuitée</td>
-			</tr>
-			<tr>
 			    <td><strong>Eté 2024</strong><br>01 juin 24 - 31 août 24*</td>
 			    <td>390&euro; par nuitée</td>
 			</tr>
 			<tr>
-			    <td><strong>Automne 2024</strong><br>01 septembre 24 - 31 octobre 24*</td>
+			    <td><strong>Automne 2024</strong><br>01 septembre 24 - 31 décember 24*</td>
 			    <td>330&euro; par nuitée</td>
 			</tr>
 			<tr>
-			    <td><strong>Standard 2025</strong><br>01 janvier 25 - 11 avril 25*</td>
-			    <td>290&euro; par nuitée</td>
+			    <td><strong>Standard 2025</strong><br>01 janvier 25 - 31 mars 25*</td>
+			    <td>370&euro; par nuitée</td>
 			</tr>
 			<tr>
-			    <td><strong>Printemps 2025</strong><br>12 avril 25 - 31 mai 25*</td>
-			    <td>330&euro; par nuitée</td>
-			</tr>
-			<tr>
-			    <td><strong>Eté 2025</strong><br>01 juin 25 - 31 août 25*</td>
+			    <td><strong>Printemps 2025</strong><br>01 avril 25 - 31 mai 25*</td>
 			    <td>390&euro; par nuitée</td>
 			</tr>
 			<tr>
-			    <td><strong>Automne 2025</strong><br>01 septembre 25 - 31 octobre 25*</td>
-			    <td>330&euro; par nuitée</td>
+			    <td><strong>Eté 2025</strong><br>01 juin 25 - 31 août 25*</td>
+			    <td>410&euro; par nuitée</td>
+			</tr>
+			<tr>
+			    <td><strong>Automne 2025</strong><br>01 septembre 24 - 31 décember 24*</td>
+			    <td>390&euro; par nuitée</td>
+			</tr>
+			<tr>
+			    <td><strong>Standard 2025</strong><br>01 novembre 25 - 31 décember 25*</td>
+			    <td>370&euro; par nuitée</td>
 			</tr>
 		</tbody>
 	</table>
--- templates/block-table--pricing.php
+++ templates/block-table--pricing.php
@@ -7,36 +7,32 @@
 		</thead>
 		<tbody>
 			<tr>
-			    <td><strong>Standard Season 2024</strong><br>01 January 2024 - 22 March 2024 (Easter)*</td> 
-			    <td>&euro;290 per night</td>
-			</tr>
-			<tr>
-			    <td><strong>Spring 2024</strong><br>23 March 2024 - 31 May 2024*</td>
-			    <td>&euro;330 per night</td>
-			</tr>
-			<tr>
 			    <td><strong>Summer 2024</strong><br>01 June 2024 - 31 August 2024*</td>
 			    <td>&euro;390 per night</td>
 			</tr>
 			<tr>
-			    <td><strong>Autumn 2024</strong><br>01 September 2024 - 31 October 2024*</td>
+			    <td><strong>Autumn 2024</strong><br>01 September 2024 - 31 December 2024*</td>
 			    <td>&euro;330 per night</td>
 			</tr>
 			<tr>
-			    <td><strong>Standard Season 2025</strong><br>01 January 2025 - 11 April 2025 (Easter)*</td>
-			    <td>&euro;290 per night</td>
+			    <td><strong>Standard Season 2025</strong><br>01 January 2025 - 31 March 2025*</td>
+			    <td>&euro;370 per night</td>
 			</tr>
 			<tr>
-			    <td><strong>Spring 2025</strong><br>12 April 2025 - 31 May 2025*</td>
-			    <td>&euro;330 per night</td>
-			</tr>
-			<tr>
-			    <td><strong>Summer 2025</strong><br>01 June 2025 - 31 August 2025*</td>
+			    <td><strong>Spring 2025</strong><br>01 April 2025 - 31 May 2025*</td>
 			    <td>&euro;390 per night</td>
 			</tr>
 			<tr>
+			    <td><strong>Summer 2025</strong><br>01 June 2025 - 31 August 2025*</td>
+			    <td>&euro;410 per night</td>
+			</tr>
+			<tr>
 			    <td><strong>Autumn 2025</strong><br>01 September 2025 - 31 October 2025*</td>
-			    <td>&euro;330 per night</td>
+			    <td>&euro;390 per night</td>
+			</tr>
+			<tr>
+			    <td><strong>Standard Season 2025</strong><br>01 November 2025 - 31 December 2025*</td>
+			    <td>&euro;370 per night</td>
 			</tr>
 		</tbody>
 	</table>
