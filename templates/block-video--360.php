78089fcc78fc4223b6cfd82f74778b51d1ff36d4
Robin Green <robin@designingreen.co.uk>
2026-02-12 10:40:36 +0000

    Update to guesthouse

---
+++ templates/block-video--360.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="small-12 medium-centered medium-10 large-9 columns">
+
+		<div>
+			<iframe src='https://www.hdmedia.fr/visite-virtuelle/hd/cbp8KprUyb-la-mouissone-06-grasse.html' width='100%'
+			height='450' scrolling=no frameborder=0 allowfullscreen='true' allowvr='yes' allow='autoplay; fullscreen; xr-spatial-
+			tracking'> </iframe>
+			<!-- <br/>
+			<a title='Visite virtuelle' alt='Visite virtuelle' href='https://www.hdmedia.fr'>Visite virtuelle HD Media</a>
+			-->
+		</div>
+		
+	</div>
+</div><!-- end .row -->
